3d1e3180b2a30eed7cccd3e30eafffac340dfcf2
hurricanejay <jaywen89@gmail.com>
2020-07-16 18:21:29 -0400

    Done.

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
 
+
 class RestaurantInput extends Component {
+  state = {
+    text: ''
+  }
+
+  handleChange = (e) => {
+    this.setState({text: e.target.value})
+  } 
+
+  submitChange = (e) => {
+    e.preventDefault()
+    this.props.addRestaurant(this.state.text)
+    this.setState({text: ''})
+  }
+
+
   render() {
     return (
       <div>
-        Restaurant Input
+        <form htmlFor='restaurant-input' onSubmit={(e) =>this.submitChange(e)}>
+          <label>Restaurant Name</label>
+          <input type='text' value={this.state.text} onChange={(e)=>this.handleChange(e)} />
+        <input type='submit'/>
+        </form>
       </div>
     );
   }
 };
 
-export default RestaurantInput;
+
+export default (RestaurantInput);
--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -2,13 +2,25 @@ import React, { Component } from 'react';
 import Review from './Review';
 
 class Reviews extends Component {
+
   render() {
+
+    const { reviews, restaurantId, deleteReview } = this.props;
+    const associatedReviews = reviews.filter(review => review.restaurantId === restaurantId);
+    
+    const reviewList = associatedReviews.map((review, index) => {
+      return <Review key={index} review={review} deleteReview={deleteReview} />
+    })
+
     return (
-      <ul>
-        Reviews
-      </ul>
+      <div>
+        <ul>
+          {reviewList}
+        </ul>
+      </div>
     );
   }
+
 };
 
 export default Reviews;
